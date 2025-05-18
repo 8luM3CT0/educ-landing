5df0e5c0dc267c6fe9a9aaca7884b27d9e396c15
Robert Cloa <rumlowb@gmail.com>
2025-05-17 23:27:53 -0700

    commit

--- components/for-desktop/body/management-div/ManagementDiv.js
+++ components/for-desktop/body/management-div/ManagementDiv.js
@@ -1,6 +1,7 @@
 //front-end
 import React from 'react'
 //back-end
+import {useRef, useState} from 'react'
 import { courses } from '../../..'
 
 function ManagementDiv({
@@ -14,6 +15,8 @@ function ManagementDiv({
     setCloseViewMentor,
     scrollRef
 }) {
+  const [selectedMentor, setSelectedMentor] = useState(null)
+
   return (
     <>
      <div 
@@ -73,7 +76,10 @@ function ManagementDiv({
                             </h3>
                             <button 
                             disabled={viewMentor}
-                            onClick={() => setViewMentor(true)}
+                            onClick={() => {
+                              setSelectedMentor(lecturer);
+                              setViewMentor(true);
+                            }}
                             className="
                             h-[40%]
                             w-[85%]
@@ -102,7 +108,7 @@ function ManagementDiv({
                     <div className="
                     h-[50%] w-[95%] mx-auto bg-slate-900 bg-opacity-20 rounded-lg
                     ">
-                      {(viewMentor || closeViewMentor) && (
+                      {((viewMentor || closeViewMentor) && selectedMentor !== null) && (
                         <div 
                         className={`
                           h-full 
@@ -120,12 +126,13 @@ function ManagementDiv({
                             if(closeViewMentor){
                               setViewMentor(false)
                               setCloseViewMentor(false)
+                              setSelectedMentor(null)
                             }
                           }}
                           >
                             <span 
                             style={{
-                              backgroundImage: `url(${'https://i.pinimg.com/736x/3d/85/a0/3d85a04596ee3070cffb5362bffa7202.jpg'})`
+                              backgroundImage: `url(${selectedMentor?.lecpic || ''})`
                             }}
                             className="h-full w-[50%] bg-cover bg-no-repeat "></span>
                             <span className="
@@ -148,7 +155,7 @@ function ManagementDiv({
                               ">
                                 <span className="w-full px-3 py-3 bg-slate-800 bg-opacity-70">
                                 <h3 className="font-font-robot font-semibold text-lg text-slate-100">
-                                  Mentor Name
+                                  {selectedMentor?.name}
                                 </h3>
                                 </span>
                                 <span className="
@@ -160,17 +167,7 @@ function ManagementDiv({
                               bg-opacity-30
                                 ">
                                   <p className="font-montserr text-base text-slate-100 font-bold px-3 py-2">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt pretium augue, in lacinia felis. Nunc vel dui ante. Nunc non odio commodo, feugiat lorem ac, interdum massa. Aenean neque nibh, rutrum non tempor quis, sollicitudin nec nulla. Etiam ut arcu bibendum, euismod nisl eget, facilisis dui. Donec malesuada imperdiet dui, eget porta ipsum ultrices sit amet. Nunc efficitur lacinia mi, eu suscipit quam ultrices sit amet. In elementum urna in mattis tristique. Aliquam non enim sapien. Vestibulum fringilla, dolor non tristique venenatis, purus felis iaculis mauris, condimentum blandit eros magna pulvinar orci. Suspendisse nec massa eleifend, finibus nunc eget, suscipit ex. Suspendisse potenti. Suspendisse dignissim erat diam.
-
-Phasellus dapibus nibh elementum ipsum placerat interdum. Nullam venenatis ut augue at congue. Phasellus vehicula lacus nec nisl luctus pharetra. Duis aliquet sit amet velit non tempor. Suspendisse pellentesque neque enim, ut aliquet erat hendrerit vitae. In ac tempus sapien. Suspendisse tristique, ex vitae condimentum blandit, elit elit faucibus turpis, vel consequat quam nunc pellentesque ex. Vestibulum dolor sem, rhoncus id metus et, egestas fringilla arcu.
-
-Vestibulum vel erat eu lectus venenatis pellentesque. Quisque luctus rutrum tempus. Nulla dignissim mi nec maximus imperdiet. Fusce velit nisl, lacinia sed magna non, posuere mollis mauris. Donec viverra ullamcorper lectus. Proin consequat euismod magna. Cras pharetra semper gravida. Curabitur sit amet imperdiet mi. In suscipit eleifend odio vitae pretium. Proin iaculis tempor imperdiet. Sed gravida quis massa nec dictum. Nam et dui dictum metus facilisis semper vel in erat. Nunc malesuada tellus sit amet orci sodales, a porttitor est malesuada. Pellentesque pharetra lectus condimentum, tincidunt odio eget, rutrum mauris. Morbi rhoncus, sem nec aliquet fringilla, dolor ipsum feugiat neque, non ultrices augue nisi a enim.
-
-Sed vel augue in erat pulvinar fermentum sagittis a est. Ut dignissim massa erat, id fermentum lorem aliquet id. Morbi placerat porta nisi id euismod. Aliquam condimentum nisi eu nisl viverra, vel dapibus orci bibendum. Maecenas et tellus maximus enim laoreet ultricies. Maecenas consequat maximus metus vel ornare. Vivamus non velit augue. Aenean condimentum luctus odio ac dictum.
-
-Fusce elit purus, hendrerit at pretium a, pellentesque nec metus. Etiam pretium semper purus, et placerat nisi posuere nec. Sed id volutpat nisi. In justo tellus, imperdiet elementum consectetur quis, ornare id risus. Fusce hendrerit ipsum eget lectus lacinia posuere. Proin facilisis id erat laoreet blandit. Phasellus eu diam a lectus imperdiet finibus. Donec commodo magna eget libero consectetur, vel tristique neque porta. Praesent at laoreet eros. Vestibulum congue luctus ligula. Vestibulum et ultricies massa.
-
-
+                                    {selectedMentor?.background}
                                   </p>
                                 </span>
                               </span>
